5e52bdb4e6882c93600c9009ba142713bfa1450e
Eric Youngson <eric@scneco.com>
2015-08-06 12:53:01 -0700

    Added blank python header file for re-writing script

---
+++ surfcheck.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2014 Eric Allen Youngson
+
+"""This module is intended to mimick the functionalit of the perl script:
+    surfcheck.pl by kurtwindisch@yahoo.com"""
+# Written by Eric Youngson eric@scneco.com / eayoungs@gmail.com
+# Succession Ecological Services: Portland, Oregon
+
