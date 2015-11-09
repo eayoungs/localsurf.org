49bb209df472901ec8bf526ea10828b574d5ad8f
Eric Youngson <eric@scneco.com>
2015-11-09 12:15:20 -0800

    branch: master Intent: Copy URLs used in Perl script to Python file for reference ================================================================== **surfcheck.py** - Added URLs to get started... - Added web URLs for main NOAA data site... - Added sample usage of token: Required for access limiting...
    **.gitignore**
    - Added commit log file to hide from repo

--- surfcheck.py
+++ surfcheck.py
@@ -5,3 +5,30 @@
 # Written by Eric Youngson eric@scneco.com / eayoungs@gmail.com
 # Succession Ecological Services: Portland, Oregon
 
+
+import requests
+
+
+# **********************************************************************
+# From: NOAA Web svs http://www.ncdc.noaa.gov/cdo-web/webservices/v2
+# **********************************************************************
+
+# Primary API  for noaa Climate Data Online [CDO]
+# http://www.ncdc.noaa.gov/cdo-web/api/v2/{endpoint}
+#
+# Sample usage (requires token):
+# curl -H "token:<token>" url
+# $.ajax({ url:<url>, data:{<data>}, headers:{ token:<token> } })
+
+# **********************************************************************
+# From: oregonsurfcheck.com Perl script (surfcheck.pl)
+# **********************************************************************
+
+# NWS_Coastal_Primary_URL
+"http://www.wrh.noaa.gov/pqr"
+"http://www.wrh.noaa.gov/total_forecast/marine.php?marine=PZZ255"
+
+# NWS_Coastal_Data_URL
+"http://www.wrh.noaa.gov/total_forecast/marine.php?marine=PZZ255"
+
+
